2c3d004aae11978236819248f60ec14b96051782
astro-mat <whoismatwarren@gmail.com>
2024-04-27 10:58:07 +0000

    Add basic post function

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-7#7kc92!zlcy8_fvdnw69@i)tt56!#7q4u$^)yw^hb%cglef34
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['8000-astromat-djangoblogwalk-wuo94onztaa.ws-eu110.gitpod.io','.herokuapp.com']
 
 
@@ -87,6 +87,12 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com"
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
